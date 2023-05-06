a850c8bebaa6c262d9ce4009362974d0de3fcd00
ssalpo <ssalpomishevich@gmail.com>
2023-05-06 12:58:05 +0500

    фикс

--- app/Services/NomenclatureArrivalService.php
+++ app/Services/NomenclatureArrivalService.php
@@ -30,20 +30,4 @@ class NomenclatureArrivalService
 
         return $nomenclatureArrival;
     }
-
-    public function getNomenclatureGroupedByPrice(string $searchQuery = null): Collection|array
-    {
-        return NomenclatureArrival::selectRaw('nomenclature_arrivals.nomenclature_id, nomenclatures.name, nomenclature_arrivals.price_for_sale')
-            ->join(
-                'nomenclatures', fn($q) => $q->on('nomenclatures.id', '=', 'nomenclature_arrivals.nomenclature_id')
-            )
-            ->where('nomenclature_arrivals.company_id', auth()->user()?->company_id)
-            ->when(
-                $searchQuery,
-                fn($q, $v) => $q->where('nomenclature_arrivals.price_for_sale', 'like', '%' . $v . '%')
-                    ->orWhere('nomenclatures.name', 'like', '%' . $v . '%')
-            )
-            ->groupBy(DB::raw('nomenclature_arrivals.nomenclature_id, nomenclature_arrivals.price_for_sale'))
-            ->get();
-    }
 }
