b1c3c5b212bbdf8071aa3bc3cf36ed3b39fa898b
ssalpo <ssalpomishevich@gmail.com>
2023-05-07 12:06:43 +0500

    учет списания и возврата номенклатуры

--- app/Services/StorehouseBalanceService.php
+++ app/Services/StorehouseBalanceService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\NomenclatureArrival;
+use App\Models\NomenclatureOperation;
 use App\Models\Order;
 use App\Models\OrderItem;
 use Illuminate\Support\Facades\DB;
@@ -23,6 +24,15 @@ class StorehouseBalanceService
             ->groupBy('nomenclature_id')
             ->get();
 
+        $nomenclatureOperations = NomenclatureOperation::select(
+            'type',
+            'nomenclature_id',
+            DB::raw('SUM(quantity) as quantity')
+        )
+            ->groupBy('type')
+            ->groupBy('nomenclature_id')
+            ->get();
+
         return NomenclatureArrival::select(
             'nomenclature_id',
             DB::raw('n.name AS nomenclature_name'),
@@ -30,13 +40,20 @@ class StorehouseBalanceService
         )->join('nomenclatures as n', 'n.id', '=', 'nomenclature_arrivals.nomenclature_id')
             ->groupBy('nomenclature_id')
             ->get()
-            ->transform(function ($m) use ($orderItems) {
+            ->transform(function ($m) use ($orderItems, $nomenclatureOperations) {
                 $orderItem = $orderItems->where('nomenclature_id', $m->nomenclature_id)->first();
+                $nomenclatureWithdraw = $nomenclatureOperations->where('nomenclature_id', $m->nomenclature_id)
+                    ->where('type', NomenclatureOperation::OPERATION_TYPE_WITHDRAW)->first();
+
+                $nomenclatureRefund = $nomenclatureOperations->where('nomenclature_id', $m->nomenclature_id)
+                    ->where('type', NomenclatureOperation::OPERATION_TYPE_REFUND)->first();
+
+                $subtractQuantity = ($orderItem?->quantity ?? 0) + ($nomenclatureWithdraw?->quantity ?? 0);
 
                 return [
                     'id' => $m->nomenclature_id,
                     'nomenclature_name' => $m->nomenclature_name,
-                    'quantity' => $m->quantity - ($orderItem?->quantity ?? 0)
+                    'quantity' => ($m->quantity + ($nomenclatureRefund?->quantity ?? 0)) - $subtractQuantity
                 ];
             });
     }
